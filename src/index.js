f2710f75de3a56eeb764c02a563473ef4cc9a56a
ramyNiranjan <ramy.niranjan@gmail.com>
2021-05-30 22:59:03 +0200

    initail setup

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer } = require("apollo-server");
+
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
+};
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
